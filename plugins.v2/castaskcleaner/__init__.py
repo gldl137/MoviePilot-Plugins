f9c2feada61cf15349ec7f7ace21a4537d04a7d1
gldl137 <54521752+gldl137@users.noreply.github.com>
2025-07-02 14:42:10 +0800

    Update __init__.py

--- plugins.v2/castaskcleaner/__init__.py
+++ plugins.v2/castaskcleaner/__init__.py
@@ -45,7 +45,7 @@ class CASTaskCleaner(_PluginBase):
     # 插件图标
     plugin_icon = "cloud189.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "gldl137"
     # 作者主页
